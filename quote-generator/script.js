acca3dee84745b599be39fd7acf4a9a54d44c2bc
Mo Nash <psinassh@gmail.com>
2022-02-18 12:41:20 +0100

    quote generator design

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,1 +1,17 @@
-alert("Hello")
+function loading() {
+    const quoteContent = document.getElementById("quote-content")
+    const loader = document.getElementById("loader")
+
+    quoteContent.classList.add("d-none")
+    loader.classList.remove("d-none")
+}
+
+function showQuote(){
+    const loader = document.getElementById("loader")
+    const quoteContent = document.getElementById("quote-content")
+
+    loader.classList.add("d-none")
+    quoteContent.classList.remove("d-none")
+}
+loading();
+setTimeout(showQuote,1500)
