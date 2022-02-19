73997b2a5a8ea38efb5dcb9afe392f6d7206c74d
Mo Nash <psinassh@gmail.com>
2022-02-19 12:46:51 +0100

    screen capture

--- picture-in-picture screen capture/script.js
+++ picture-in-picture screen capture/script.js
@@ -1,13 +1,30 @@
 const start=document.getElementById("start")
 const video=document.getElementById("video")
+var captureStream =null;
 
+const captureContraint= {
+    width: { min: 640, ideal: 1280, max: 1920 },
+    height: { min: 720, ideal: 720, max: 1080 }
+}
 if ('pictureInPictureEnabled' in document) {
    
     start.hidden = false;
   
     start.addEventListener('click', () => {
-      video.requestPictureInPicture();
+        captureStream=navigator.mediaDevices.getDisplayMedia();
+        captureStream.then((mediaStream)=>{
+            console.log(mediaStream)
+            video.srcObject=mediaStream;
+            console.log(video.srcObject)
+            video.addEventListener("loadedmetadata",()=>{
+                console.log("metadata loaded")
+                video.requestPictureInPicture();
+            })
+            
+        })
+      
     });
   }
+
 
 
