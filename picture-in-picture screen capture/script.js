0b6287c126d174bcd7f1224aaf4388b782bd18aa
Mo Nash <psinassh@gmail.com>
2022-02-19 11:02:54 +0100

    implementing picture in picture

--- picture-in-picture screen capture/script.js
+++ picture-in-picture screen capture/script.js
@@ -1,1 +1,13 @@
-console.log("working....")
+const start=document.getElementById("start")
+const video=document.getElementById("video")
+
+if ('pictureInPictureEnabled' in document) {
+   
+    start.hidden = false;
+  
+    start.addEventListener('click', () => {
+      video.requestPictureInPicture();
+    });
+  }
+
+
