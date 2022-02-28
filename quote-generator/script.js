39ed8962c66270103d587071d4584765b783e09f
Mo Nash <psinassh@gmail.com>
2022-02-28 13:22:32 +0100

    cleaning quote-generator code

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,44 +1,39 @@
-function loading() {
-    const quoteContent = document.getElementById("quote-content")
-    const loader = document.getElementById("loader")
-
-    quoteContent.classList.add("d-none")
-    loader.classList.remove("d-none")
-}
-
-function showQuote() {
-    const loader = document.getElementById("loader")
-    const quoteContent = document.getElementById("quote-content")
-
-    loader.classList.add("d-none")
-    quoteContent.classList.remove("d-none")
-}
+const quoteContent = document.getElementById("quote-content")
+const spinner = document.getElementById("loader")
 // Quote generator API
 const url = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
 
+//Display loading spinner
+function displaySpinner() {
+    quoteContent.classList.add("d-none")
+    spinner.classList.remove("d-none")
+}
+
+//display Quote
+function showQuote() {
+    spinner.classList.add("d-none")
+    quoteContent.classList.remove("d-none")
+}
 
 
 function changeQuote(quote, author) {
     document.getElementById("quoteText").innerHTML = quote;
     document.getElementById("author").innerHTML = author;
-    document.getElementById("twitterSharelink").href = `https://twitter.com/intent/tweet?text="${quote}"  ${author}`
+    document.getElementById("twitterSharelink").href = `https://twitter.com/intent/tweet?text="${quote}"  ${author}`;
+
+    showQuote()
 }
 
 
 async function getQuote() {
-
-
-
-    // trigger loading
-    loading()
+    //display spinner 
+    displaySpinner()
 
     try {
+
         const response = await fetch(url);
         const data = await response.json();
-
         changeQuote(data.quoteText, data.quoteAuthor);
-
-        showQuote()
 
     } catch (error) {
         console.log(error)
@@ -47,11 +42,15 @@ async function getQuote() {
 }
 
 function loadNewQuote() {
-    loading()
+    displaySpinner()
+
+    // load 1 quote every 3 seconde
     setTimeout(() => {
         getQuote()
-    }, 3000)
+    }, 3*1000)
 
 }
 newQuote.addEventListener("click", loadNewQuote)
+
+
 getQuote()
